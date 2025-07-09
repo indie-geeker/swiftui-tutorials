eb87eee36578f5b47c29bfd4d55ceae396e88422
indiegeeker <indiegeeker@gmail.com>
2025-07-09 14:20:08 +0800

    Initial Commit

---
+++ swiftui-tutorials/swiftui_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorialsApp.swift
+//  swiftui-tutorials
+//
+//  Created by Wen on 2025/7/9.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
